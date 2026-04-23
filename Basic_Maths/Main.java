449439accda474ec1a5d38a7e0b22e88b8fe3d41
Ayushmanzzzz <ab4021@srmist.edu.in>
2026-04-23 23:53:19 +0530

    Calculated GCD of two numbers

--- Basic_Maths/Main.java
+++ Basic_Maths/Main.java
@@ -5,10 +5,13 @@ public class Main {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
-        int number = sc.nextInt();
+        // int number = sc.nextInt();
         // System.out.println(digits.digits(number));
         // System.out.println(reverse.reverse(number));
-        System.out.println(palindrome.palindrome(number));
+        // System.out.println(palindrome.palindrome(number));
+
+        int n1 = 20, n2 = 15;
+        System.out.println(GCD.findGcd(n1, n2));
         sc.close();
     }
 }
