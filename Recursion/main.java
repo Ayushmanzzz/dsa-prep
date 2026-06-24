2e8d080f41ebc5502da1cca12c3a4e5a46357e53
Ayushmanzzzz <ab4021@srmist.edu.in>
2026-06-24 19:47:03 +0530

    Add printName class to print a name N times using recursion

---
+++ Recursion/main.java
@@ -0,0 +1,10 @@
+package Recursion;
+
+public class main {
+    public static void main(String args[]){
+        String name = "Ashish";
+        int n = 5;
+
+        printName.printName(name, 0, n);
+    }
+}
