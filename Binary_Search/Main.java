dc3dc053d14bd64c9f112ee7126f3e8d595f57fa
Ayushmanzzzz <ab4021@srmist.edu.in>
2026-07-02 15:05:45 +0530

    Add implementation for finding the median of two sorted arrays using a recursive approach

---
+++ Binary_Search/Main.java
@@ -0,0 +1,13 @@
+package Binary_Search;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 3, 5};
+        int[] nums2 = {2, 4};
+
+        double median = median2SortedArrays.median2SortedArrays(nums1, nums2);
+        
+        System.out.println("The median of the two sorted arrays is: " + median);
+    }
+}
